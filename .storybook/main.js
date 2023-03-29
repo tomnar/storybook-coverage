6a9f5fe87e7cc1d4e32729532fdec3485f5e4f44
Tom Quast <tom@konsulentdb.dk>
2023-03-29 10:17:55 +0200

    Added coverage scripts

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -11,5 +13,25 @@ module.exports = {
   },
   features: {
     interactionsDebugger: true,
+  },
+  webpackFinal: async (config) => {
+    const rules = config.module?.rules || []
+    rules.push({
+      test: /\.(js|ts)$/,
+      loader: '@jsdevtools/coverage-istanbul-loader',
+      enforce: 'post',
+      include: path.join(process.cwd(), 'src'),
+      exclude: [
+        /\.(e2e|spec|stories)\.ts$/,
+        /node_modules/,
+        /(ngfactory|ngstyle)\.js/,
+        /polyfills.ts/
+      ],
+    })
+
+    config.module = config.module || {}
+    config.module.rules = rules
+
+    return config
   },
 };
